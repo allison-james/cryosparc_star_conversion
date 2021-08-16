9f0b4d9ec1dbc9abf5b85a2106978bda95cbb9f3
allison-james <68887684+allison-james@users.noreply.github.com>
2021-08-15 20:44:46 -0400

    Add files via upload

--- cryosparc_star_conversion.sh
+++ cryosparc_star_conversion.sh
@@ -8,23 +8,18 @@ read cryosparcINPUTfile
 echo "Enter job:"
 read job
 
-#initiates pyem vitual environment
 conda init bash
 conda activate pyem
 
-#run csparc2star.py
-#change path to csparc2star.py if it is not located in your home directory
-python3 ~/csparc2star.py $cryosparcINPUTfile ${job}_raw_output_particles.star -$
-
+#runs csparc2star.py from home directory
+python3 ~/csparc2star.py $cryosparcINPUTfile ${job}_raw_output_particles.star --relion2
 #remove unwanted columns
-relion_star_handler --i ${job}_raw_output_particles.star --o ${job}_output_part$
-
+relion_star_handler --i ${job}_raw_output_particles.star --o ${job}_output_particles.star --remove_column rlnRandomSubset
 #rename files in .star file
 sed -i 's/particles.mrc/particles.mrcs/' ${job}_output_particles.star
 sed -i "s/>J[0-9]*\/extract/${job}_conversion/" ${job}_output_particles.star
 echo "Enter parent (cryosparc job) file path:"
 read parent_file_path
-
 #create symbolic link to parent files
 mkdir ${job}_conversion
 cd ${job}_conversion
